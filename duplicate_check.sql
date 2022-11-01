4af14caed0a0fefb1b89e4a0f352b017984741f8
Sandeep Chintabathina <sandeep.chintabathina@gmail.com>
2022-11-01 12:59:33 -1000

    Add files via upload

--- duplicate_check.sql
+++ duplicate_check.sql
@@ -1,119 +1,84 @@
--- Queries to count records by recip_id and vax_event_id
+-- New scripts to account for all sorts of duplicate combinations 10/24/22
 
--- Identifies cdc count by grouping vax_event_id and recip_id
-select vax_event_id, recip_id from
-	mvrs_data 
-	group by vax_event_id,recip_id;
-
--- Duplication of (recip_id, admin_date) (not checked by CDC)
-select recip_id,admin_date from
-	mvrs_data 
-	group by recip_id,admin_date
-	having count(*)>1
-	order by recip_id;
-
--- Identifies duplicate (vax_event_id,recip_id) rows
-select vax_event_id,recip_id from
-		mvrs_data 
-		group by vax_event_id,recip_id
-		having count(*)>1
-		order by recip_id;
-
--- Identifies duplicates caused by (vax_event_id,recip_id) with admin_date
-select b.vax_event_id,b.recip_id,a.admin_date from
-	(select vax_event_id,recip_id from
-		mvrs_data 
-		group by vax_event_id,recip_id
-		having count(*)>1
-	)b, mvrs_data a
-	where 
-	a.vax_event_id = b.vax_event_id
-	AND a.recip_id = b.recip_id
-	order by b.recip_id;
-	
--- Identifies duplicates caused by (recip_id,admin_date)
-select vax_event_id, recip_id from 
-	mvrs_data md WHERE 
-	vax_event_id in 
-		(  select a.vax_event_id from
-			(select recip_id,admin_date from
-				mvrs_data 
-				group by recip_id,admin_date
-				having count(*)>1
-			)b, mvrs_data a
-			WHERE 
-			b.admin_date = a.admin_date
-			and b.recip_id = a.recip_id
-		)
-		order by recip_id;
-
---Same as above different approach
--- Adding grouping to remove repetitions
-select a.vax_event_id,a.recip_id from
-		    mvrs_data a join
-			(select recip_id,admin_date from
-				mvrs_data 
-				group by recip_id,admin_date
-				having count(*)>1
-			)b
-			on  b.admin_date = a.admin_date
-			and b.recip_id = a.recip_id
-			group by a.vax_event_id,a.recip_id
-			order by b.recip_id;
-
--- Compares each record in DB against the duplicate set
--- to return list of records that match on recip_id (and admin_date) but not on vax_event_id
-select md.vax_event_id,md.recip_id
+-- Script to get records that have 
+-- duplicate vax_event_id,recip_id,admin_date values
+select vax_event_id, recip_id, admin_date
+			from mvrs_data 
+			group by vax_event_id,recip_id,admin_date 
+			having count(*)>1
+			order by vax_event_id;
+		
+-- Script with similar objective as above
+-- But this helps to break down duplicates counts by 2 or 3 
+-- change count(*)>1 or count(*)>2 to get various duplicate counts
+select md.vax_event_id, md.recip_id, md.admin_date 
 	from mvrs_data md
-	join (
-		 select a.vax_event_id,a.recip_id from
-		    mvrs_data a join
-			(select recip_id,admin_date from
-				mvrs_data 
-				group by recip_id,admin_date
-				having count(*)>1
-			)b
-			on  b.admin_date = a.admin_date
-			and b.recip_id = a.recip_id
-			group by a.vax_event_id,a.recip_id
-		) c
-	on
-	md.vax_event_id <> c.vax_event_id
-	and 
-	md.recip_id = c.recip_id 
+	join (select vax_event_id, recip_id, admin_date
+			from mvrs_data 
+			group by vax_event_id,recip_id,admin_date 
+			having count(*)>1)dup
+    on
+    	md.vax_event_id = dup.vax_event_id
+    	and md.recip_id = dup.recip_id
+    	and md.admin_date = dup.admin_date
+	order by md.vax_event_id;
+
+
+-- Script to get records that have
+-- duplicate vax_event_id, recip_id but not admin_date
+select md.vax_event_id, md.recip_id, md.admin_date
+	from mvrs_data md 
+	join (select b.vax_event_id,b.recip_id,b.admin_date  -- to get admin_date add this layer
+			from mvrs_data b
+			join 
+			(select vax_event_id,recip_id
+				from mvrs_data 
+				group by vax_event_id, recip_id
+				having count(*)>1) dup
+			on b.vax_event_id = dup.vax_event_id
+			and b.recip_id = dup.recip_id ) a
+	on 
+		md.vax_event_id = a.vax_event_id
+		and md.recip_id = a.recip_id
+		and md.admin_date != a.admin_date
 	order by md.recip_id;
 
+-- Script to get records that have
+-- duplicate recip_id,admin_date but not vax_event_id
+select md.vax_event_id, md.recip_id, md.admin_date
+	from mvrs_data md 
+	join (select b.vax_event_id,b.recip_id,b.admin_date   -- to get the vax_event_id add this layer
+			from mvrs_data b
+			join 
+			(select recip_id, admin_date
+				from mvrs_data 
+				group by recip_id,admin_date 
+				having count(*)>1) dup
+			on b.recip_id = dup.recip_id
+			and b.admin_date = dup.admin_date) a
+	on 
+		md.recip_id = a.recip_id
+		and md.admin_date = a.admin_date
+		and md.vax_event_id!=a.vax_event_id   -- non matching vax_event_id
+	order by md.recip_id ;
+	
 
--- Instead of comparing with all records in DB with duplicate set
--- Compare identical duplicate sets and pick the ones that do not match on vax_event_id
-select md.vax_event_id,md.recip_id,md.admin_date
-	from ( 
-			select d.vax_event_id,d.recip_id,d.admin_date from
-		    mvrs_data d join
-			(select recip_id,admin_date from
-				mvrs_data 
-				group by recip_id,admin_date
-				having count(*)>1
-			)e
-			on  e.admin_date = d.admin_date
-			and e.recip_id = d.recip_id
-			group by d.vax_event_id,d.recip_id
-	     ) md
-	join (
-		 select a.vax_event_id,a.recip_id from
-		    mvrs_data a join
-			(select recip_id,admin_date from
-				mvrs_data 
-				group by recip_id,admin_date
-				having count(*)>1
-			)b
-			on  b.admin_date = a.admin_date
-			and b.recip_id = a.recip_id
-			group by a.vax_event_id,a.recip_id
-		) c
-	on
-	md.vax_event_id <> c.vax_event_id  --mismatching vax_event ids
-	and 
-	md.recip_id = c.recip_id 
-	order by md.recip_id;
-
+-- Script to get records that have
+-- duplicate vax_event_id, admin_date but not recip_id
+select md.vax_event_id, md.recip_id, md.admin_date
+	from mvrs_data md 
+	join (select b.vax_event_id,b.recip_id,b.admin_date   -- to get the recip_id add this layer
+			from mvrs_data b
+			join 
+			(select vax_event_id,admin_date 
+				from mvrs_data 
+				group by vax_event_id,admin_date 
+				having count(*)>1)dup
+			on b.vax_event_id = dup.vax_event_id
+			and b.admin_date = dup.admin_date ) a
+	on 
+		md.vax_event_id = a.vax_event_id
+		and md.admin_date = a.admin_date
+		and md.recip_id != a.recip_id
+	order by md.vax_event_id;
+	
